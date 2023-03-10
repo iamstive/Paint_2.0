bed8f6fbff89c68e3d5ce53fd169c93bfbec30dc
iamstive <117988180+iamstive@users.noreply.github.com>
2023-03-10 17:44:48 +0500

    Update Paint_20.py

--- Paint_20.py
+++ Paint_20.py
@@ -5,6 +5,35 @@ import pandas as pd
 import random
 
 
+max0 = [0, 0, 0, 0]
+min0 = [10, 10, 10, 10]
+max1 = [0, 0, 0, 0]
+min1 = [10 ,10, 10, 10]
+max2 = [0, 0, 0, 0]
+min2 = [10, 10, 10, 10]
+p_ta = []
+p_tt = []
+for i in range(len(y_train)):
+    if y_train[i] == 0:
+        max0 = [max(max0[0], x_train[i][0]), max(max0[1], x_train[i][1]), max(max0[2], x_train[i][2]), max(max0[3], x_train[i][3])]
+        min0 = [min(max0[0], x_train[i][0]), min(max0[1], x_train[i][1]), min(max0[2], x_train[i][2]), min(max0[3], x_train[i][3])]
+    if y_train[i] == 1:
+        max1 = [max(max1[0], x_train[i][0]), max(max1[1], x_train[i][1]), max(max1[2], x_train[i][2]), max(max1[3], x_train[i][3])]
+        min1 = [min(max1[0], x_train[i][0]), min(max1[1], x_train[i][1]), min(max1[2], x_train[i][2]), min(max1[3], x_train[i][3])]
+    if y_train[i] == 2:
+        max2 = [max(max2[0], x_train[i][0]), max(max2[1], x_train[i][1]), max(max2[2], x_train[i][2]), max(max2[3], x_train[i][3])]
+        min2 = [min(max2[0], x_train[i][0]), min(max2[1], x_train[i][1]), min(max2[2], x_train[i][2]), min(max2[3], x_train[i][3])]
+for i in range(5000):
+    q = random.randint(0, 3)
+    if q == 0:
+        p_tt.append([random.randint(int(min0[0]*100), int(max0[0]*100))/100, random.randint(int(min0[1]*100), int(max0[1]*100))/100, random.randint(int(min0[2]*100), int(max0[2]*100))/100, random.randint(int(min0[3]*100), int(max0[3]*100))/100])
+        p_ta.append(0)
+    if q == 1:
+        p_tt.append([random.randint(int(min1[0]*100), int(max1[0]*100))/100, random.randint(int(min1[1]*100), int(max1[1]*100))/100, random.randint(int(min1[2]*100), int(max1[2]*100))/100, random.randint(int(min1[3]*100), int(max1[3]*100))/100])
+        p_ta.append(1)
+    if q == 2:
+        p_tt.append([random.randint(int(min2[0]*100), int(max2[0]*100))/100, random.randint(int(min2[1]*100), int(max2[1]*100))/100, random.randint(int(min2[2]*100), int(max2[2]*100))/100, random.randint(int(min2[3]*100), int(max2[3]*100))/100])
+        p_ta.append(2)
 x_train, x_test, y_train, y_test = train_test_split(iris_dataset['data'], iris_dataset['target'], random_state=0)
 def calculator(res1, res2, res3, res4):
     l = list()
